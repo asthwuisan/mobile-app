ee3ae194b161df29cf5cc0aeb0704669d39acb76
asthwuisan <asthriedwuisan09@gmail.com>
2025-09-01 20:33:29 +0800

    exercise 2

--- App.tsx
+++ App.tsx
@@ -1,28 +1,42 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- */
-
-import { NewAppScreen } from '@react-native/new-app-screen';
-import { StatusBar, StyleSheet, useColorScheme, View } from 'react-native';
-
-function App() {
-  const isDarkMode = useColorScheme() === 'dark';
-
-  return (
-    <View style={styles.container}>
-      <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
-      <NewAppScreen templateFileName="App.tsx" />
-    </View>
-  );
-}
+import React from 'react';
+import { Image, StyleSheet } from 'react-native';
+import { SafeAreaView, SafeAreaProvider } from 'react-native-safe-area-context';
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    paddingBottom: 100,
+  },
+  tinyLogo: {
+    width: 50,
+    height: 50,
+  },
+  logo: {
+    width: 66,
+    height: 58,
   },
 });
 
-export default App;
+const DisplayAnImage = () => (
+  <SafeAreaProvider>
+    <SafeAreaView style={styles.container}>
+      <Image source={require('./assets/local.jpg')} />
+      <Image
+        // style={styles.tinyLogo}
+        source={{
+          uri: 'https://tse3.mm.bing.net/th/id/OIP.KLutQzmurWnk0bFujxyQkgHaFg?pid=Api&P=0&h=180',
+        }}
+      />
+      <Image
+        style={styles.logo}
+        source={{
+          uri:
+            'data:image/png;base64,' +
+            'iVBORw0KGgoAAAANSUhEUgAAAAoAAAAKCAYAAACNMs+9AAAAMUlEQVQoU2NkYGBgYGBg+A8EDAwMDAwMjI2N/4EwCkbGxsYGhgYmRkZGBgYGBjZgQMAAGdcBAX9U3sQAAAAAElFTkSuQmCC',
+        }}
+      />
+    </SafeAreaView>
+  </SafeAreaProvider>
+);
+
+export default DisplayAnImage;
