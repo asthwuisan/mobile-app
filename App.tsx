7a7ba50aa7329b980e529079378b98ef831a05e8
asthwuisan <asthriedwuisan09@gmail.com>
2025-09-10 22:20:40 +0800

    exercise-4

--- App.tsx
+++ App.tsx
@@ -1,28 +1,51 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- */
+import React from 'react';
+import { Text, StyleSheet, Image, View } from 'react-native';
 
-import { NewAppScreen } from '@react-native/new-app-screen';
-import { StatusBar, StyleSheet, useColorScheme, View } from 'react-native';
-
-function App() {
-  const isDarkMode = useColorScheme() === 'dark';
-
+// Komponen utama
+const App = () => {
   return (
     <View style={styles.container}>
-      <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
-      <NewAppScreen templateFileName="App.tsx" />
+      {/* Foto Profil dari file lokal */}
+      <Image source={require('./assets/fotoprofil.jpg')} style={styles.image} />
+
+      {/* Biodata Sederhana */}
+      <Text style={styles.name}>Asthried Tesalonika Wuisan</Text>
+      <Text style={styles.info}>
+        Tempat, Tanggal Lahir: Manado, 9 Maret 2003
+      </Text>
+      <Text style={styles.info}>Alamat: Watutumou III Blok O No 6</Text>
+      <Text style={styles.info}>Email: asthriedwuisan09@gmail.com</Text>
+      <Text style={styles.info}>Telepon: 085824845071 </Text>
+      <Text style={styles.info}>Status: Mahasiswa</Text>
     </View>
   );
-}
+};
+
+export default App;
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#7777e3ff',
+    padding: 20,
+  },
+  image: {
+    width: 150,
+    height: 150,
+    borderRadius: 75,
+    marginBottom: 20,
+    borderWidth: 2,
+    borderColor: '#333',
+  },
+  name: {
+    fontSize: 22,
+    fontWeight: 'bold',
+    marginBottom: 10,
+  },
+  info: {
+    fontSize: 16,
+    marginBottom: 5,
   },
 });
-
-export default App;
---
+++ Exercise4.tsx
@@ -0,0 +1,104 @@
+import React, { useState } from 'react';
+import {
+  StyleSheet,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  View,
+  SafeAreaView,
+} from 'react-native';
+import { SafeAreaProvider } from 'react-native-safe-area-context';
+
+const Exercise3: React.FC = () => {
+  const [username, setUsername] = useState<string>('');
+  const [password, setPassword] = useState<string>('');
+
+  const handleLogin = () => {
+    console.log('Username:', username);
+    console.log('Password:', password);
+  };
+
+  return (
+    <SafeAreaProvider>
+      <SafeAreaView style={styles.container}>
+        {/* Title */}
+        <Text style={styles.welcome}>Welcome</Text>
+
+        {/* Username */}
+        <View style={styles.inputContainer}>
+          <Text style={styles.label}>Username</Text>
+          <TextInput
+            style={styles.input}
+            placeholder="Masukan username anda"
+            value={username}
+            onChangeText={setUsername}
+          />
+        </View>
+
+        {/* Password */}
+        <View style={styles.inputContainer}>
+          <Text style={styles.label}>Password</Text>
+          <TextInput
+            style={styles.input}
+            placeholder="Masukan password anda"
+            secureTextEntry
+            value={password}
+            onChangeText={setPassword}
+          />
+        </View>
+
+        {/* Sign In Button */}
+        <TouchableOpacity style={styles.button} onPress={handleLogin}>
+          <Text style={styles.buttonText}>Sign In</Text>
+        </TouchableOpacity>
+      </SafeAreaView>
+    </SafeAreaProvider>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'flex-start',
+    paddingHorizontal: 20,
+    backgroundColor: '#ffffffff',
+    paddingTop: 20,
+  },
+  welcome: {
+    fontSize: 30,
+    fontWeight: 'bold',
+    marginBottom: 40,
+    color: '#000',
+  },
+  inputContainer: {
+    marginBottom: 20,
+  },
+  label: {
+    fontSize: 16,
+    fontWeight: 'bold',
+    marginBottom: 6,
+    color: '#333',
+  },
+  input: {
+    height: 45,
+    borderWidth: 1,
+    borderColor: '#ccc',
+    borderRadius: 6,
+    paddingHorizontal: 12,
+    backgroundColor: '#fff',
+  },
+  button: {
+    backgroundColor: '#FF6600',
+    paddingVertical: 14,
+    borderRadius: 6,
+    alignItems: 'center',
+    marginTop: 20,
+  },
+  buttonText: {
+    color: '#fff',
+    fontSize: 16,
+    fontWeight: '600',
+  },
+});
+
+export default Exercise3;
